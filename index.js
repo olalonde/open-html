6adb9f18a7803ec41789a57b5b8a9762bbc7b3f4
Michal Doubek <michal@doubkovi.cz>
2020-08-09 13:46:59 +0200

    Add support for Windows and use Node.js OS to dynamically find tmp folder to be more platform independent.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+var os = require('os');
 var fs = require('fs'),
   exec = require('child_process').exec;
 
 function open(html) {
-  var tmpPath = '/tmp/node-open-webpage-' + new Date().getTime() + '.html';
+  var tmpPath = os.tmpdir() + '/node-open-webpage-' + new Date().getTime() + '.html';
   fs.writeFileSync(tmpPath, html);
-  exec('open ' + tmpPath);
+
+  if (process.platform === 'win32')
+    exec('start ' + tmpPath);
+
+  else
+    // MacOS
+    exec('open ' + tmpPath);
 }
 
 module.exports = open;
